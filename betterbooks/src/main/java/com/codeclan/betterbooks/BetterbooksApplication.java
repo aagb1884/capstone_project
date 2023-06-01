9a2e667a50092f713d50b18765dd735cec6d682e
Andrew Blair <a.a.g.blair@gmail.com>
2023-06-01 15:12:52 +0100

    added spring folder

---
+++ betterbooks/src/main/java/com/codeclan/betterbooks/BetterbooksApplication.java
@@ -0,0 +1,13 @@
+package com.codeclan.betterbooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetterbooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetterbooksApplication.class, args);
+	}
+
+}
---
+++ betterbooks/src/test/java/com/codeclan/betterbooks/BetterbooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeclan.betterbooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetterbooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
