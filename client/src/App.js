72dacbbaa36b5dab55cfceea34021ca9424f92ae
Andrew Blair <a.a.g.blair@gmail.com>
2023-06-09 11:20:58 +0100

    made a personally satisfying home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import HomePage from './Components/HomePage';
 import Container from './Containers/container';
 
 
@@ -6,6 +7,7 @@ function App(){
 
   return (
     <div className="App">
+      <HomePage />
   <Container />
   </div>
   );
