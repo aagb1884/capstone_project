c04be099e13eb547ee8cda11fc88dcfb654fe398
Andrew Blair <a.a.g.blair@gmail.com>
2023-06-09 11:26:43 +0100

    realised cool homepage thing would need to be intensively responsive and i do not have time for that right now

--- client/src/Components/HomePage.js
+++ client/src/Components/HomePage.js
@@ -4,12 +4,9 @@ const HomePage = () => {
     return ( 
         <>
         <h1><i>Slightly</i> Better Reads</h1>
-        <div class="container">
         <a href="link address"><img SRC="icons8-open-book.gif" alt="book pages gif" width="110" height="110"/></a>
-            <div class="centered1">Enter</div>
-            <div class="centered2">Here</div>
-        </div>
-        
+        <p><a href="link address">Enter Here</a></p>
+       
         </>
      );
 }
