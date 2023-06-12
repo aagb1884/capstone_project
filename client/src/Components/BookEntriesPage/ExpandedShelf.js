9d438db7722dc5769763f27a084f11c7aa3b89d1
Andrew Blair <a.a.g.blair@gmail.com>
2023-06-12 14:24:19 +0100

    data on book entries page

--- client/src/Components/BookEntriesPage/ExpandedShelf.js
+++ client/src/Components/BookEntriesPage/ExpandedShelf.js
@@ -1,23 +1,26 @@
+import './expandedshelf.css'
+
 const ExpandedShelf = ({bookEntry}) => {
 
 // map through lists of authorrs and illustrators after lunch
 
     return ( 
+        <div className='expanded-shelf'>
         <ul>
             <li><img src={bookEntry.book.coverUrl} alt="Book cover" width="175" height="200"/></li>
             <li>{bookEntry.book.title}</li>
-            <li>{bookEntry.book.authors.firstname} {bookEntry.book.authors.lastname}</li>
-            <li>{bookEntry.book.illustrators.firstname} {bookEntry.book.illustrators.lastname}</li>
-            <li>{bookEntry.starRating}</li>
-            <li>{bookEntry.book.synopsis}</li>
-            <li>{bookEntry.book.lengthInPages}</li>
-            <li>{bookEntry.book.format}</li>
-            <li>{bookEntry.book.datePublished}</li>
-            <li>{bookEntry.book.publisher}</li>
-            <li>{bookEntry.book.isbn}</li>
-            <li>{bookEntry.book.averageRating}</li>
-
+            <li>Written by: {bookEntry.book.authors.firstname} {bookEntry.book.authors.lastname}</li>
+            <li>Illustrated by:{bookEntry.book.illustrators.firstname} {bookEntry.book.illustrators.lastname}</li>
+            <li>Star Rating: {bookEntry.starRating}</li>
+            <li>Synopsis: {bookEntry.book.synopsis}</li>
+            <li>No. Pages: {bookEntry.book.lengthInPages}</li>
+            <li>Format: {bookEntry.book.format}</li>
+            <li>Date Published: {bookEntry.book.datePublished}</li>
+            <li>Publisher: {bookEntry.book.publisher}</li>
+            <li>ISBN: {bookEntry.book.isbn}</li>
+            <li>Average Rating: {bookEntry.book.averageRating}</li>
         </ul>
+        </div>
      );
 }
  
