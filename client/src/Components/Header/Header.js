b624bb26112db912b3ed43b4c26a2802bf045b5c
Andrew Blair <a.a.g.blair@gmail.com>
2023-06-12 21:18:40 +0100

    burger menu added

--- client/src/Components/Header/Header.js
+++ client/src/Components/Header/Header.js
@@ -1,14 +1,33 @@
 import './header.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 const Header = () => {
+
+
+
     return (
        <div className='header'> 
        <h2>Slightly</h2>
         <div className='better-reads-header'>
                 <h1>Better Reads</h1>
-            </div>
-        </div>
+            </div>         
+                <div className="dropdown">
+                    <button class="dropbtn"><FontAwesomeIcon icon={faBars} style={{color: "#ffffff",}} size="2x" /></button>
+                        <div className="dropdown-content">
+                            <ul>
+                            <li><a rel='noopener' href="/">Home</a></li>
+                            <li><a rel='noopener' href="/mybooks">My Books</a></li>
+                            <li><a rel='noopener' href="/books">All Books</a></li>
+                            <li><a rel='noopener' href="/search" >Search</a></li>
+                            <li><a rel='noopener' href="/profile" >User Profile</a></li>
+                            <li><a rel='noopener' href="/newbookentry">Add Book to Shelf</a></li>
+                            </ul>
+                        </div>
+                </div>
+            </div>      
       );
-}
- 
+    }
+
+
 export default Header;
